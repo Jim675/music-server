bdd815bde8589c4c2dea285dcb551cb4c3a4fe97
Jim675 <56530557+Jim675@users.noreply.github.com>
2023-03-17 22:15:07 +0800

    添加启动类注释

--- music-server/src/main/java/com/example/yin/YinMusicApplication.java
+++ music-server/src/main/java/com/example/yin/YinMusicApplication.java
@@ -4,6 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @description: 启动类
+ * @author: qw Lu
+ * @date: 2023/03/17 22:14:42
+ **/
 @SpringBootApplication
 @MapperScan("com.example.yin.mapper")
 public class YinMusicApplication {
